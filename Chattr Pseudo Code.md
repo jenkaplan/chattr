fc3c1a9565f7875d5083e4accbfe3f73226866e5
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-20 00:22:48 -0500

    add passport config, local strategy, auth helpers

---
+++ Chattr Pseudo Code.md
@@ -0,0 +1,11 @@
+# Chattr Pseudo Code
+1. user creates an account
+2. user logs in
+3. user sees past messages
+3. user sends message
+4. user views other messages
+5. messages past 24 hours are erased
+
+
+## Past MVP
+- look into Twilio api
---
+++ auth/passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const models = require('../db/models/index');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  });
+
+  passport.deserialzeUser((id, done) => {
+    models.User.findById(id)
+    .then((user) => { done(null, user); })
+    .catch((err) => { done(err, null); });
+  });
+};

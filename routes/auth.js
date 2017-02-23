4ac3200e8997f106c060edc57262db74fcf38a69
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-23 11:24:02 -0500

    updated ejs files, broken partials

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
+// requires the Express node
 const express = require('express');
+// requires the Express router node
 const router = express.Router();
 
+// requires the authHelpers file
 const authHelpers = require('../auth/auth-helpers');
+// requires the local file
 const passport = require('../auth/local');
 
+// renders the register page
 router.get('./register', authHelpers.loginRedirect, (req, res) => {
   res.render('auth/register');
 });
 
+// allows the user the create an account and sends the data to the user table
 router.post('/register', (req, res, next) => {
   return authHelpers.createUser(req, res)
   .then((response) => {
@@ -16,17 +22,20 @@ router.post('/register', (req, res, next) => {
   .catch((err) => { res.status(500).json({ status: 'error' }); });
 });
 
+// loads the login page
 router.get('/login', authHelpers.loginRedirect, (req, res) => {
   res.render('auth/login');
 });
 
+// route handler based on if the user is logged in
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/user',
+    successRedirect: '/messages',
     failureRedirect: '/auth/login',
     failureFlash: true
   })
 );
 
+// logout
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
---
+++ routes/messages.js
@@ -0,0 +1,41 @@
+// requires the Express node
+const express = require('express');
+// requires the Express router node
+const router = express.Router();
+
+// calls in the database
+var models = require('../db/models/index');
+// calls in auth-helpers file
+var authHelpers = require('../auth/auth-helpers');
+
+authHelpers.loginRequired,
+// route to the the messages page
+router.get('/', function(req, res, next) {
+  models.Meals.findAll({}).then(function(messages) {
+    res.render('messages/index', {
+      messages:messages
+    });
+  });
+});
+
+// posts data from the message form into the message table
+router.post('/', function(req, res, next) {
+  models.Messages.create({
+    message: req.body.message
+  }).then(function (messages) {
+    res.redirect('/messages')
+  });
+});
+
+// deletes the data when a user clicks delete
+router.delete('/:id', function(req, res, next) {
+  models.Messages.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(messages) {
+    res.redirect('/messages');
+  });
+});
+
+module.exports = router;

77a44c4b93be7fa06ac55984d83a8cd93850a43d
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-25 14:48:51 -0500

    moved socket functions

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const authRoutes = require('./routes/auth.js');
 const userRoutes = require('./routes/user.js');
 const messagesRoutes = require('./routes/messages.js');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 require('dotenv').config();
 
@@ -34,6 +36,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 app.use('/', index);
 app.use('/auth', authRoutes);
--- routes/messages.js
+++ routes/messages.js
@@ -2,8 +2,6 @@
 const express = require('express');
 // requires the Express router node
 const router = express.Router();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 // calls in the database
 var models = require('../models/index');
@@ -30,26 +28,16 @@ router.post('/', function(req, res, next) {
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+// deletes the data when a user clicks delete
+router.post('/:id', function(req, res, next) {
+  models.Messages.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(messages) {
+    res.redirect('/messages');
   });
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-// deletes the data when a user clicks delete
-// router.post('/:id', function(req, res, next) {
-//   models.Messages.destroy({
-//     where: {
-//       id: req.params.id
-//     }
-//   }).then(function(messages) {
-//     res.redirect('/messages');
-//   });
-// });
 
 function autoDelete(req, res, next) {
   models.sequelize.query('DELETE "Message".* FROM "Messages" WHERE "createdAt" < Now()')

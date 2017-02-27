714accb2f2e66c2d6e4a926623e884ed5300c819
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-27 09:47:23 -0500

    fixed messages ejs

--- app.js
+++ app.js
@@ -12,10 +12,22 @@ const authRoutes = require('./routes/auth.js');
 const userRoutes = require('./routes/user.js');
 const messagesRoutes = require('./routes/messages.js');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 require('dotenv').config();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,19 +49,6 @@ app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/messages', function(req, res){
-//   res.sendFile(__dirname + '/routes/messages.js');
-// });
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 app.use('/', index);
 app.use('/auth', authRoutes);
